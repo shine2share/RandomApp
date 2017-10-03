373befab2332d7a01f6be2b73bcc1056af83763e
thelam92 <thelam92@gmail.com>
2017-10-03 21:03:49 +0700

    Add check uesr input.

--- app/src/main/java/come/wordpress/share2study/myapplication/MainActivity.java
+++ app/src/main/java/come/wordpress/share2study/myapplication/MainActivity.java
@@ -2,10 +2,12 @@ package come.wordpress.share2study.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
@@ -30,11 +32,21 @@ public class MainActivity extends AppCompatActivity {
         btnRandom.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                int number1 = Integer.parseInt(edtNumber1.getText().toString());
-                int number2 = Integer.parseInt(edtNumber2.getText().toString());
-                Random random = new Random();
-                int number = random.nextInt((number2-number1)+1)+number1;
-                txtNumber.setText(number + "");
+
+                    String text1 = edtNumber1.getText().toString().trim();
+                    String text2 = edtNumber2.getText().toString().trim();
+                if (text1.length()==0 || text2.length() == 0) {
+                    Toast toast = Toast.makeText(getApplicationContext(), "You must type a number.", Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 1, 5);
+                    toast.show();
+                } else {
+                    int number1 = Integer.parseInt(text1);
+                    int number2 = Integer.parseInt(text2);
+                    Random random = new Random();
+                    int number = random.nextInt((number2 - number1) + 1) + number1;
+                    //txtNumber.setText(number + "");
+                    txtNumber.setText(String.valueOf(number));
+                }
             }
         });
 
